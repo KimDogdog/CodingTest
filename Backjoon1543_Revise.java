dd766cff1c7f3ae3c97ba4c436e2f77894a4b618
wlals3236 <wlals3236@naver.com>
2022-09-28 20:16:50 +0900

    Backjoon1543

--- Backjoon1543_Revise.java
+++ Backjoon1543_Revise.java
@@ -6,8 +6,8 @@ public class Backjoon1543_Revise {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		String document = sc.nextLine(); //문서 문자열 입력받기
-		String searchString = sc.nextLine(); //찾을 문자열 입력받기 
+		String document = sc.nextLine(); //문서 문자열 입력받음
+		String searchString = sc.nextLine(); //찾을 문자열 입력음 
 		
 		int documentNum = document.length(); //문서 문자열의 문자열개수
 		document = document.replace(searchString,""); //replace함수를 이용해서 찾을 문자열을 공백으로 교체
