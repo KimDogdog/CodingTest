43a1b03a988fb4f4c6b8c313cced8bf1a8eafedb
wlals3236 <wlals3236@naver.com>
2022-09-29 22:36:43 +0900

    Baekjoon1292

---
+++ Baekjoon1292.java
@@ -0,0 +1,33 @@
+package codingTest;
+
+import java.util.Scanner;
+
+public class Baekjoon1292 {
+	public static void main(String[] agrs) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		int sum = 0;
+		int count = 1;
+	
+		for(int i = 1; i<= 1000;i++){
+			for(int j = 1; j<=i; j++) {
+				if(a <=count&&count <=b  ) {
+					sum += i; 
+					count++;
+				}
+				else if(b < count) {
+					break;
+				}
+				else {
+					count++;					
+				}
+			}
+		}
+		
+		System.out.println(sum);
+		sc.close();
+	}
+}
